2504f7982806fc34c75f86027289b825255e9196
rohan <purohit.rohan@gmail.com>
2020-01-20 23:58:39 +0530

    Add Custom Sneeky Analog Clock
    Thanks to mohancm
    Thanks to ROGERdotT for thumbnail.
    
    Change-Id: I704a991ee5301b3ab3a4514b542b44406a9d71ce
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -174,6 +174,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new StickerTagClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new TuxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new LineageClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new SneekyClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();

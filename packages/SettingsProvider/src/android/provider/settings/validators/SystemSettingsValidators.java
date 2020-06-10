2d9611f013d5f070f6eb0489732b76e9e6dc9d30
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-10 21:37:59 +0300

    base: Allow to flash on call only when entirely silent [1/3]
    Change-Id: If5a3e9730c07f56b2564b56667da5390eb8fa446

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -262,6 +262,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.FP_WAKE_UNLOCK, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.LOCKSCREEN_BATTERY_INFO, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.QS_SYSTEM_INFO, ANY_INTEGER_VALIDATOR);
-        VALIDATORS.put(System.FLASHLIGHT_ON_CALL, new InclusiveIntegerRangeValidator(0, 3));
+        VALIDATORS.put(System.FLASHLIGHT_ON_CALL, new InclusiveIntegerRangeValidator(0, 4));
     }
 }

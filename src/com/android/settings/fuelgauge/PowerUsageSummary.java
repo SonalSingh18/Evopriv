Project-Xtended/packages_apps_Settings

Smart Features: add config for making it optional [2/2]
SDB: unify with respect to frameworks/base

Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -273,7 +273,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
         // Check availability of Smart Features
         mSmartFeaturesCat = (PreferenceCategory) findPreference(KEY_SMART_FEATURES_CATEGORY);
-        if (!getResources().getBoolean(R.bool.config_supportSmartFeatures)) {
+        if (!getResources().getBoolean(com.android.internal.R.bool.config_supportSmartFeatures)) {
             getPreferenceScreen().removePreference(mSmartFeaturesCat);
         }
     }

SonalSingh18/Evopriv

Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -278,7 +278,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
         // Check availability of Adaptive Charging
         mAdaptiveChargingCat = (PreferenceCategory) findPreference(KEY_ADAPTIVE_CHARGING_CATEGORY);
-        if (!getResources().getBoolean(R.bool.config_supportAdaptiveCharging)) {
+        if (!getResources().getBoolean(com.android.internal.R.bool.config_supportAdaptiveCharging)) {
             getPreferenceScreen().removePreference(mAdaptiveChargingCat);
         }
     }